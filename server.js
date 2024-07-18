6a02b822bbccaf3a96d40c70c231c2cc2b282256
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-07-18 17:01:45 -0500

    finally got the server back working again added the config folder with connection js file

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const sequelize = require('./config/connection');
+//require files from express and connection js files
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log('server started!')
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () =>{
+        console.log("server Started!");
+    })
 });
