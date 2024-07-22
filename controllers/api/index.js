fff7aab8a52ee0ea8b96677b492822a9ae59a03f
hannahschwen <hannahschwen@gmail.com>
2024-07-22 11:13:06 -0400

    moved bookRoutes file, connect to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const bookRoutes = require('./bookRoutes');
 
 router.use('/users', userRoutes)
+router.use('/book', bookRoutes)
 
 // this is just here as a test
 router.get('/', async (req, res) => {
