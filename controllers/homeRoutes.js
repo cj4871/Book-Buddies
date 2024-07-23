e2b7c58cf4edfb6dac1e5c0a18dac5cd65455dea
hannahschwen <hannahschwen@gmail.com>
2024-07-23 09:54:24 -0400

    started setting up handlebars
    kinda starting to set up handlebars just so I can make a form to work out the login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response -- which has not been created yet
-  // res.render('homepage');
+  res.render('homepage');
   res.send(`You're on controllers/homeRoutes file`);
 });
 
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
-const routes = require('./controllers')
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 //require files from express and connection js files
 
 const app = express();
 const PORT = process.env.PORT || 5432;
+
+// Set up handlebars with helpers
+const hbs = exphbs.create({ helpers });
+
+//for handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
