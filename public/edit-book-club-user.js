e381f76c4c2cf93861fa4877d8257d22144cdb0e
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-07-23 12:05:01 -0500

    added public folder with a couple of things in it

---
+++ public/edit-book-club-user.js
@@ -0,0 +1,7 @@
+async function newBookClubUser(event){
+    event.preventDefault();
+    // add const to pull users as well as logged in user
+    const first_name = document.querySelector('#first_name')
+    //checks if the user is online. 
+    const online_status = 
+}
