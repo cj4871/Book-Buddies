54b3af399b0688ef406f64e82142c682d3813815
hannahschwen <hannahschwen@gmail.com>
2024-07-23 10:50:50 -0400

    added auth.js, and login redirect to profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,6 +9,11 @@ router.get('/profile', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  // if logged in, go to profile instead
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
   res.render('login');
 });
 
