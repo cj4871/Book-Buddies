0b10919f7982e156527f0c4fcc98cf9a12462f14
hannahschwen <hannahschwen@gmail.com>
2024-07-23 12:07:29 -0400

    add event listeners to form buttons

--- public/js/login.js
+++ public/js/login.js
@@ -43,3 +43,7 @@ const signupFormHandler = async (event) => {
     }
   }
 };
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
