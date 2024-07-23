257d694231aa961c8b391af8ac608b63be91db19
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-07-22 20:29:56 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,12 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
-
-//allow for sequelize to access .env file with the password and postgres
-const sequelize = new Sequelize(
-    process.env.BOOKS_DB, 
-    process.env.BOOKS_USER,
-    process.env.BOOKS_PW,
-    {
-    host: 'localhost',
-    dialect: 'postgres'
-    }
-)
-//export sequelize to other files
+const Sequelize = require('sequelize');
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.BOOKS_DB, process.env.BOOKS_USER, process.env.BOOKS_PW, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 module.exports = sequelize;
