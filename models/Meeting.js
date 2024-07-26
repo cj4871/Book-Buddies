1612b30332d8edee25b5ca91d0584fffa2bc15de
Adam <aorf@femmeosage.net>
2024-07-26 17:40:52 -0500

    working on backend stuff and book search page

--- models/Meeting.js
+++ models/Meeting.js
@@ -1,54 +1,55 @@
-const { Model, DataTypes } = require('sequelize')
-const sequelize = require('../config/connection')
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Meeting extends Model {};
+class Meeting extends Model {}
 
-//Definiton for Meeting model 
+//Definiton for Meeting model
 Meeting.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-        date:{
-            type: DataTypes.DATEONLY,
-            allowNull: false,
-            },
-        location: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        time: {
-            type: DataTypes.TIME,
-            allowNull: false,
-        },
-        book_chapter: {
-            type: DataTypes.INTEGER,
-        },
-        // BookClub_id: {
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //         model: 'BookClub',
-        //         key: 'id',
-        //     },
-        // },
-        // Book_id:{
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //         model: 'book',
-        //         key: 'id',
-        //     },
-        // },
-        },
-    {
-        sequelize,
-        modelName: 'Meeting',
-        freezeTableName: true,
-        underscored: true,
-        timestamps: true,
-        }
-    );
-  
-  module.exports = Meeting;
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    book_chapter: {
+      type: DataTypes.INTEGER,
+    },
+
+    // BookClub_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: 'BookClub',
+    //         key: 'id',
+    //     },
+    // },
+    // Book_id:{
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: 'book',
+    //         key: 'id',
+    //     },
+    // },
+  },
+  {
+    sequelize,
+    modelName: "Meeting",
+    freezeTableName: true,
+    underscored: true,
+    timestamps: true,
+  }
+);
+
+module.exports = Meeting;
