29a9d7ef5a045b5d1b6aea83bec87d3c99b5304c
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-07-28 13:29:16 -0500

    still working on this

--- public/js/add-book-club-user.js
+++
@@ -1,4 +0,0 @@
-const { response } = require("express");
-const { User } = require("../../models");
-
-const club_search = document.querySelector('#search-bar');
--- public/js/remove-club-user.js
+++ public/js/remove-club-user.js
@@ -1,1 +1,18 @@
 const removeUser = document.querySelector("#remove_btn")
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('User_id')) {
+        const id = event.target.getAttribute('User_id');
+        const response = await fetch(`/api/User/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/User');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  fetch()
